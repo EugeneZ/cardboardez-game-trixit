c7c68d1d4259f8f41692ca10c1bde5e4914a4430
EugeneZ <eugene.zar@gmail.com>
2016-09-26 18:27:55 -0700

    Improving welcome screen

--- client/component/Welcome.js
+++ client/component/Welcome.js
@@ -4,13 +4,13 @@ import FontIcon from 'material-ui/FontIcon';
 import Paper from 'material-ui/Paper';
 
 export default ()=>
-    <Paper style={{ padding: '5%' }}>
-        <div style={{ display: 'flex', marginBottom: 50 }}>
+    <div style={{ padding: '5%' }}>
+        <div style={{ display: 'flex', marginBottom: 50, flexWrap: 'wrap' }}>
             <RaisedButton primary={true} label="Login with Google" href="/auth/google"
-                          icon={<FontIcon className="fa fa-google"/>} style={{ margin: 'auto' }}/>
+                          icon={<FontIcon className="fa fa-google"/>} style={{ margin: '10px auto' }}/>
             <RaisedButton primary={true} label="Login with Facebook" href="/auth/facebook"
-                          icon={<FontIcon className="fa fa-facebook"/>} style={{ margin: 'auto' }}/>
+                          icon={<FontIcon className="fa fa-facebook"/>} style={{ margin: '10px auto' }}/>
             <RaisedButton primary={true} label="Login with Github" href="/auth/github"
-                          icon={<FontIcon className="fa fa-github"/>} style={{ margin: 'auto' }}/>
+                          icon={<FontIcon className="fa fa-github"/>} style={{ margin: '10px auto' }}/>
         </div>
-    </Paper>;
+    </div>;
